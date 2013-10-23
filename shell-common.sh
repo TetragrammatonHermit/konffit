d750f00fb155003194cab0eb9f37f6e64082a071
Jarkko Saltiola <jasalt@mave.lan>
2013-10-23 21:50:34 +0300

    google translate from shell instead alfred

--- shell-common.sh
+++ shell-common.sh
@@ -26,6 +26,10 @@ alias yas="yo angular:service"
 alias yaf="yo angular:filter"
 alias yad="yo angular:directive"
 
+# Google translate (https://github.com/soimort/google-translate-cli)
+alias te="trs {fi=en} " # To English
+alias tf="trs {en=fi} " # To Finnish
+
 # Paste last output
 zmodload -i zsh/parameter
 insert-last-command-output() {
@@ -33,3 +37,10 @@ insert-last-command-output() {
 }
 zle -N insert-last-command-output
 bindkey "^V" insert-last-command-output
+
+
+
+# # Translate
+# translate2(){ wget -qO- "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=$1&langpair=$2|${3:-en}" | sed 's/.*"translatedText":"\([^"]*\)".*}/\1\n/'; }
+
+# translate() { lng1="$1";lng2="$2";shift;shift; wget -qO- "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=${@// /+}&langpair=$lng1|$lng2" | sed 's/.*"translatedText":"\([^"]*\)".*}/\1\n/'; }

d4ce411ed4cd093bea79bf30af1d49fba47c714c
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-06-03 13:10:27 +0300

    Bugfix, mouse fixes on Arch.

--- linux/setmouse.sh
+++ linux/setmouse.sh
@@ -8,12 +8,18 @@ echo -n 200 > /sys/devices/platform/i8042/serio1/serio2/speed
 echo -n 255 > /sys/devices/platform/i8042/serio1/serio2/draghys
 echo -n 4 > /sys/devices/platform/i8042/serio1/serio2/mindrag
 
-#xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
-#xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
-#xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
-#xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
+
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
 
 # http://www.thinkwiki.org/wiki/How_to_configure_the_TrackPoint
 
-# Enable tap
+# Enable touchpad tap
 #synclient TapButton1=1
+
+# Disable touchpad tap
+synclient TapButton1=0
+synclient TapButton2=0
+synclient TapButton3=0

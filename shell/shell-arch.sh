c90a677285f50995fbb3ad655f5afba82f2fb268
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-07-15 17:05:49 +0300

    Aliases

--- shell/shell-arch.sh
+++ shell/shell-arch.sh
@@ -2,6 +2,7 @@ source $HOME/konffit/shell/shell-common.sh
 
 alias o="thunar ."
 alias ai="sudo pacman -S"
+alias ar="sudo pacman -R"
 alias as="sudo pacman -Ss"
 alias aii="packer -S"
 alias ass="packer -Ss"

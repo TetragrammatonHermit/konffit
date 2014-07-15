81dd4d1e9939eab0a96049e6fbb04b00257e1611
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-07-15 13:13:01 +0300

    Org-mobile, template update, refile target refinement.

--- emacs/prelude-personal/orgmode.el
+++ emacs/prelude-personal/orgmode.el
@@ -1,19 +1,27 @@
 ;; After install replace builtin org with one from elpa!
 
+;; Location of Org files on local system
+(setq org-directory "~/Dropbox/notes/")
+(setq org-default-notes-file  "~/Dropbox/notes/gtd.org")
+(setq org-agenda-files '("~/Dropbox/notes/gtd.org"))
 
-(setq org-directory "~/notes/")
-(setq org-default-notes-file  "~/notes/gtd.org")
-(setq org-agenda-files '("~/notes/gtd.org"))
-
-(setq org-mobile-directory "/Volumes/DUMPSSDER/Dropbox/Apps/MobileOrg")
-(setq org-mobile-files '("~/notes/mobiletest.org" "~/notes/gtd.org"))
-(setq org-mobile-inbox-for-pull "~/notes/incoming.org")
+;; Org-mobile setup
+(setq org-mobile-files '("~/Dropbox/notes/gtd.org"))
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+;; File where new mobile-notes will be stored
+(setq org-mobile-inbox-for-pull "~/Dropbox/notes/mobile-incoming.org")
 
 
 (setq org-use-fast-todo-selection t)
 (setq org-use-fast-tag-selection t)
 (setq org-catch-invisible-edits 'smart)
+(setq org-ctrl-k-protect-subtree t)
 
+
+(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+(setq org-refile-use-outline-path t)
+(setq org-outline-path-complete-in-steps t)
+;;(setq org-refile-allow-creating-parent-nodes t)
 (prelude-require-package 'org-trello)
 
 ;; Emacs global keybindings
@@ -65,25 +73,24 @@
 (setq org-capture-templates
       '(
         ;; Org-protocol
-        ("w" "" (file+headline "~/notes/www.org" "Notes") "* %^{Title}\n\n  Source: %u, %c\n\n  %i")
-        ("t" "Task / Chore" entry (file+headline "~/notes/gtd.org" "Tasks")
+        ("t" "Task / Chore" entry (file+headline "~/Dropbox/notes/gtd.org" "Task / Chore")
          "* TODO %?\n  %i\n")
-        ("m" "Calendar" entry (file+headline "~/notes/gtd.org" "Tasks") "* APPT %?\n  %i\n")
-        ("r" "Raw thought" entry (file+headline "~/notes/gtd.org" "Ideas")
+        ("m" "Calendar" entry (file+headline "~/Dropbox/notes/gtd.org" "Calendar") "* APPT %?\n  %i\n")
+        ("r" "Raw thought" entry (file+headline "~/Dropbox/notes/gtd.org" "Raw thought")
          "* %?\n  %i\n")
-        ("b" "Buy" entry (file+headline "~/notes/todo.org" "Task / Chore")
+        ("b" "Buy" entry (file+headline "~/Dropbox/notes/todo.org" "Task / Chore")
          "* TODO Osta %?\n  %i\n")
-        ("o" "Tweaks to OS" entry (file+headline "~/notes/someday.org" "OS Tweak") "* %?\n  %i\n")
-        ("s" "Song" item (file+headline "~/notes/music.org" "New music")
+        ("o" "Tweaks to OS" entry (file+headline "~/Dropbox/notes/someday.org" "OS Tweak") "* %?\n  %i\n")
+        ("s" "Song" item (file+headline "~/Dropbox/notes/music.org" "New music")
          "%?\n  %i\n")
-        ("j" "Journal entry" entry (file+datetree "~/notes/notetoself.org")
+        ("j" "Journal entry" entry (file+datetree "~/Dropbox/notes/notetoself.org")
          "* %?\nEntered on %U\n  %i\n")
-        ("d" "Dream" entry (file+headline "~/notes/notetoself.org" "Dreams") "* %U\n  %i\n%?")))
+        ("d" "Dream" entry (file+headline "~/Dropbox/notes/notetoself.org" "Dreams") "* %U\n  %i\n%?")))
 
 
 (setq org-agenda-custom-commands
       '(("c" "Desk Work" tags "HOME" ;; (1) (2) (3) (4)
-         ((org-agenda-files '("~/notes/gtd.org")) ;; (5)
+         ((org-agenda-files '("~/Dropbox/notes/gtd.org")) ;; (5)
           (org-agenda-sorting-strategy '(priority-up effort-down))) ;; (5) cont.
          ("~/computer.html")) ;; (6)
         ;; ...other commands here
@@ -106,3 +113,32 @@
         ))
 
 (setq org-refile-targets (quote (("gtd.org" :maxlevel . 1) ("someday.org" :level . 2))))
+
+
+(add-hook 'after-init-hook 'org-mobile-pull)
+(add-hook 'kill-emacs-hook 'org-mobile-push)
+
+;; Org-mobile auto sync when idle
+(defvar my-org-mobile-sync-timer nil)
+
+(defvar my-org-mobile-sync-secs (* 60 20))
+
+(defun my-org-mobile-sync-pull-and-push ()
+  (org-mobile-pull)
+  (org-mobile-push)
+  (when (fboundp 'sauron-add-event)
+    (sauron-add-event 'my 3 "Called org-mobile-pull and org-mobile-push")))
+
+(defun my-org-mobile-sync-start ()
+  "Start automated `org-mobile-push'"
+  (interactive)
+  (setq my-org-mobile-sync-timer
+        (run-with-idle-timer my-org-mobile-sync-secs t
+                             'my-org-mobile-sync-pull-and-push)))
+
+(defun my-org-mobile-sync-stop ()
+  "Stop automated `org-mobile-push'"
+  (interactive)
+  (cancel-timer my-org-mobile-sync-timer))
+
+(my-org-mobile-sync-start)

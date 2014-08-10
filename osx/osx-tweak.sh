142e477c548d832944a07ab911f45a09d5082521
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-08-10 13:17:29 +0300

    Fix terminal characters on emacs.

--- osx/osx-tweak.sh
+++ osx/osx-tweak.sh
@@ -3,3 +3,6 @@ defaults write com.apple.dashboard mcx-disabled -boolean NO
 
 # Disable safe sleep (save 16gb space, fall asleep faster)
 sudo pmset hibernatemode 0
+
+# Fix emacs terminal
+tic -o ~/.terminfo /Applications/Emacs.app/Contents/Resources/etc/e/eterm-color.ti

966ef4712107fe682f69eb9959703ab9d01708ec
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-08-27 14:40:02 +0300

    Fix inheritance for rPi debian setup.

--- shell/shell-debian.sh
+++ shell/shell-debian.sh
@@ -1,3 +1,6 @@
 # plugins=(git autojump debian npm python pip pyenv screen sudo web-search extract)
+source $HOME/konffit/shell/shell-common.sh
+source $HOME/konffit/shell/shell-linux.sh
+
 alias aup="sudo apt-get update"
 alias aug="sudo apt-get upgrade"
--- shell/shell-xubuntu.sh
+++ shell/shell-xubuntu.sh
@@ -1,5 +1,3 @@
-source $HOME/konffit/shell/shell-common.sh
 source $HOME/konffit/shell/shell-debian.sh
-source $HOME/konffit/shell/shell-linux.sh
 
 alias o="thunar ."

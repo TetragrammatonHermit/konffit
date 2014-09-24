6ac9cdc2301cb3f61c8f7e55e4daf4c41e170a6f
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-09-24 11:10:37 +0300

    Workaround for Xubuntu 14.04 screen lock bug.

---
+++ linux/ubuntu-scripts/99-lock-screen-suspend-fix.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Lock xscreensaver on resume from a suspend.
+
+# getXuser gets the X user belonging to the display in $displaynum.
+# If you want the foreground X user, use getXconsole!
+getXuser() {
+        user=`pinky -fw | awk '{ if ($2 == ":'$displaynum'" || $(NF) == ":'$displaynum'" ) { print $1; exit; } }'`
+        if [ x"$user" = x"" ]; then
+                startx=`pgrep -n startx`
+                if [ x"$startx" != x"" ]; then
+                        user=`ps -o user --no-headers $startx`
+                fi
+        fi
+        if [ x"$user" != x"" ]; then
+                userhome=`getent passwd $user | cut -d: -f6`
+                export XAUTHORITY=$userhome/.Xauthority
+        else
+                export XAUTHORITY=""
+        fi
+        export XUSER=$user
+}
+
+if pidof xscreensaver > /dev/null; then
+    for x in /tmp/.X11-unix/*; do
+        displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+        getXuser;
+        if [ x"$XAUTHORITY" != x"" ]; then
+            export DISPLAY=":$displaynum"
+            case "$1" in
+                resume|thaw)
+                    sudo -u $XUSER xscreensaver-command -unthrottle
+                ;;
+                suspend|hibernate)
+                    sudo -u $XUSER xscreensaver-command -throttle
+                    sudo -u $XUSER xscreensaver-command -lock
+                ;;
+            esac
+        fi
+    done
+fi
